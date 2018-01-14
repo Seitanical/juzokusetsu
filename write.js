77d44274d0cf05d83164b94519a3a3f3686e77ed
Seitanical <seitanical@gmail.com>
2018-01-14 21:03:56 +0100

    somewhat functional now

--- write.js
+++ write.js
@@ -4,16 +4,16 @@ var coordinate =  [];
 var test1 = {
 	sentence : "私は映画を見ることが好きです。",
 	nouns : [ //we need to catalogue the different word types in this sentence and make them findable
-	[1, 1, 0], //Last value determines whether or not the value has already been correctly chosen
-	[3, 4, 0], //this also denotes range, first value is start, second is endpoint from base 1, not 0
-	[8, 9, 0]
+	[1, 1, false], //Last value determines whether or not the value has already been correctly chosen
+	[3, 4, false], //this also denotes range, first value is start, second is endpoint from base 1, not 0
+	[8, 9, false]
 	],
 	particles : [
-	[2, 2, 0],
-	[5, 5, 0], 
-	[10, 10, 0]
+	[2, 2, false],
+	[5, 5, false], 
+	[10, 10, false]
 	],
-	juzoku : [3, 7, 0] //this is the real answer to the question and coincides with 映画を見る
+	juzoku : [3, 7, false] //this is the real answer to the question and coincides with 映画を見る
 };
 
 document.writeln(test1.sentence + '<br>');
@@ -36,11 +36,26 @@ for (i = 0; i < test1.particles.length; i++) {
 	coordinate = []
 };
 
-for (i = 0; i < test1.juzoku.length; i++) { 
-	coordinate.push(test1.juzoku[i]);
+// call this function to see if selected text is identical to the answer
+function answer (){
+	for (i = 0; i < test1.juzoku.length; i++) { 
+		coordinate.push(test1.juzoku[i]);
+	};
+	return test1.sentence.slice(coordinate[0] - 1, coordinate[1]);
 };
-document.writeln(test1.sentence.slice(coordinate[0] - 1, coordinate[1])+ '<br>');
+
+// go through verification of the juzokusetsu
+function verify (){
+	var selectedText = window.getSelection().toString();
+	var verification = answer();
+	if (selectedText === verification) {
+		document.writeln("Correct");	
+		test1.juzoku[3] = true
+	} else {
+		document.writeln("wrong");
+	} ;
 	coordinate = []
+};
 
 //Place the following in the button function
 //var result1; //Do an IF to see if it's correct or not.
