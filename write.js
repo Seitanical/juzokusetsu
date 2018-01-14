b7a86873713394528a29a825ee3a7c860159b442
Seitanical <seitanical@gmail.com>
2018-01-14 16:24:21 +0100

    can recreate catalog of nouns and particles

--- write.js
+++ write.js
@@ -1,29 +1,49 @@
-var answer = window.getSelection().toString();
-document.writeln(answer);
+var i,j;
+var coordinate =  [];
 
 var test1 = {
 	sentence : "私は映画を見ることが好きです。",
-	//nouns : ["私".position : 1, "映画".position : 3, "こと".position : 8],
-	particles : ["は", "を", "が"],
-	particlesPosition : [2, 5, 10]
+	nouns : [ //we need to catalogue the different word types in this sentence and make them findable
+	[1, 1, 0], //Last value determines whether or not the value has already been correctly chosen
+	[3, 4, 0], //this also denotes range, first value is start, second is endpoint from base 1, not 0
+	[8, 9, 0]
+	],
+	particles : [
+	[2, 2, 0],
+	[5, 5, 0], 
+	[10, 10, 0]
+	],
+	juzoku : [3, 7, 0] //this is the real answer to the question and coincides with 映画を見る
 };
 
-var i;
+document.writeln(test1.sentence + '<br>');
 
-document.writeln(test1.sentence);
 
-//document.writeln(test1.nouns.length);
-
-document.writeln(test1.particles.length);
-
-//for (i = 0; i < test1.nouns.length; i++) {
-//	document.writeln(test1.nouns[i])
-//};
-
-for (i = 0; i < test1.particles.length; i++) {
-	document.writeln(test1.particles[i])
+//this recreates a list of words based on word-type
+for (i = 0; i < test1.nouns.length; i++) { 
+	for (j= 0; j < test1.nouns.length - 1; j++ ){
+	coordinate.push(test1.nouns[i][j]);
+	};
+	document.writeln(test1.sentence.slice(coordinate[0] - 1, coordinate[1])+ '<br>');
+	coordinate = []
 };
+
+for (i = 0; i < test1.particles.length; i++) { 
+	for (j= 0; j < test1.particles.length - 1; j++ ){
+	coordinate.push(test1.particles[i][j]);
+	};
+	document.writeln(test1.sentence.slice(coordinate[0] - 1, coordinate[1]) + '<br>');
+	coordinate = []
+};
+
+for (i = 0; i < test1.juzoku.length; i++) { 
+	coordinate.push(test1.juzoku[i]);
+};
+document.writeln(test1.sentence.slice(coordinate[0] - 1, coordinate[1])+ '<br>');
+	coordinate = []
+
 //Place the following in the button function
-var result1; //Do an IF to see if it's correct or not.
-
-document.getElementById("result").innerHTML = result1;
+//var result1; //Do an IF to see if it's correct or not.
+//document.getElementById("result").innerHTML = result1;
+//var answer = window.getSelection().toString();
+//document.writeln(answer);
