169befc4f96ab7f72d36928eeb060f52b935bb8b
Seitanical <seitanical@gmail.com>
2018-01-14 13:01:50 +0100

    progress

--- write.js
+++ write.js
@@ -1,3 +1,6 @@
+var answer = window.getSelection().toString();
+document.writeln(answer);
+
 var test1 = {
 	sentence : "私は映画を見ることが好きです。",
 	//nouns : ["私".position : 1, "映画".position : 3, "こと".position : 8],
@@ -20,3 +23,7 @@ document.writeln(test1.particles.length);
 for (i = 0; i < test1.particles.length; i++) {
 	document.writeln(test1.particles[i])
 };
+//Place the following in the button function
+var result1; //Do an IF to see if it's correct or not.
+
+document.getElementById("result").innerHTML = result1;
